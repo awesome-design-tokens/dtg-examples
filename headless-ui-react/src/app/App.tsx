21f4e6a92b838c63ce4a36c8648b91ad2081728f
XOP <wdybih@gmail.com>
2023-06-10 12:19:58 +0200

    headless ui scaffolding

---
+++ headless-ui-react/src/app/App.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+
+const App = () => {
+  return (
+    <main className={styles.root}>
+      Radio Groups give you the same functionality as native HTML radio inputs,
+      without any of the styling. They're perfect for building out custom UIs
+      for selectors.
+    </main>
+  );
+};
+
+export default App;
